0a02ec857c6bb30d4c94330aba3af3eeb7ace551
Remmy Abubakar <remmybello123@gmail.com>
2024-05-08 13:31:43 +0000

    cards

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,11 +1,11 @@
-import React, { useMemo } from "react";
+import React from "react";
 import { ImagesData } from "./ImagesData";
 
 function Cards() {
-  const memoizedImages = useMemo(
-    () =>
-      ImagesData.map((item, index) => (
-        <div className="bg-white" key={index}>
+  return (
+    <div className=" font-second grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 justify-center lg:px-24 md:px-16 sm:px-1">
+      {ImagesData.map((item, index) => (
+        <div className=" bg-white" key={index}>
           <a href="#">
             <img
               className="sm:px-3 px-8 pt-8 pb-2 rounded-t-lg"
@@ -13,26 +13,20 @@ function Cards() {
               alt="product image"
             />
           </a>
-          <div className="sm:px-4 px-8">
+          <div className="sm:px-4 px-8 ">
             <a href="#">
-              <h5 className="sm:text-md md:text-lg lg:text-lg font-normal tracking-normal dark:text-[black]">
+              <h5 className="sm:text-md md:text-lg  lg:text-lg  font-normal tracking-normal dark:text-[black] ">
                 {item.title}
               </h5>
             </a>
             <div className="">
-              <div className="sm:text-md md:text-lg lg:text-lg font-second text-gray-900 dark:black">
+              <div className="sm:text-md md:text-lg lg:text-lg  font-second text-gray-900 dark:black">
                 ₵185
               </div>
             </div>
           </div>
         </div>
-      )),
-    []
-  );
-
-  return (
-    <div className="font-second grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 justify-center lg:px-24 md:px-16 sm:px-1">
-      {memoizedImages}
+      ))}
     </div>
   );
 }
